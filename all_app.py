e5eb945cf8ffa50bd4d4c3ce4d1ac662d3189f89
Yehia hesham <108961270+Yehiahesham86@users.noreply.github.com>
2025-04-22 14:39:57 +0200

    Update all_app.py

--- all_app.py
+++ all_app.py
@@ -23,7 +23,7 @@ def process_attendance_files(file):
     # Filter the data to include only the 1st to the 26th of the month
 
     # Filter the DataFrame
-     df_filtered = df[df['Date'].apply(lambda x: 1 <= x.day <= 26)]
+    df_filtered = df[df['Date'].apply(lambda x: 1 <= x.day <= 26)]
 
     # Extract Check-In and Check-Out times
     df_filtered['Check_In_Time'] = df_filtered[datetime_col].dt.time  # Extract time for Check-In
